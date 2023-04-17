fe8f389cdeddef4f46921ee7f264ab8960e58732
Raweeroj Thongdee <fewlove.007@gmail.com>
2023-04-17 20:25:31 +0700

    feat console

--- src/components/common/Console/VMConsole.tsx
+++ src/components/common/Console/VMConsole.tsx
@@ -23,17 +23,24 @@ const VMConsole: React.FC<VMConsoleProps> = ({ apiUrl }) => {
   //     fetchVncTicket()
   //   }, [apiUrl])
 
-  // Mock data for testing
   useEffect(() => {
-    // Mock data for testing
-    const data = {
-      url: 'wss://edu.ce.kmitl.cloud/api2/json/nodes/work-3/qemu/100/vncwebsocket',
-      ticket:
-        'PVEVNC:643D360D::d0UyG/TdIWJSOM1UAHrPbnCwKip6SWRNpt1f4cOgpnJvP9KrY2Y5J+wzIPDM/sCnHAdYufpk2Il7i5kyYUmujw52vxHLuAxC89TiS4soWPC7c5XcHFQ11P9nacwpH1NEzlxEIb9FpVc6vH0t9Lpf5ceYLil1XMZB254TrvMyUk18IbaNOhtqUHtmT2Wxy+UwcNPlDA7tiAc3uqjk1BCE3cPonf8BMzYKNA7tszFAksWdTzcFYnYowrbvdnDEOUriM0MyvFL57JEbQBq1kvPQuXiBZ1J9mkLqhXphXOH6blApv1U+Ndtuq4y3vlOOe4WRUaVvSdP1Zykt6T0etflviw==',
+    const fetchData = async () => {
+      let data
+      try {
+        if (apiUrl.startsWith('{')) {
+          data = JSON.parse(apiUrl)
+        } else {
+          const response = await fetch(apiUrl)
+          data = await response.json()
+        }
+        setVncTicket(data)
+      } catch (error) {
+        console.error('Error fetching VNC ticket:', error)
+      }
     }
 
-    setVncTicket(data)
-  }, [])
+    fetchData()
+  }, [apiUrl])
 
   return (
     <>
--- src/views/vm-console-page/index.tsx
+++ src/views/vm-console-page/index.tsx
@@ -8,7 +8,13 @@ import { withAuthGuard } from 'components/hocs/with-auth-guard'
 import { Page } from 'types/page'
 
 export const VMConsolePage: Page = withAuthGuard(() => {
-  const apiUrl = '/api/get-vnc-ticket' // Replace this with the actual API endpoint
+  // Mock data for testing
+  const mockData = {
+    url: 'wss://edu.ce.kmitl.cloud/api2/json/nodes/work-3/qemu/100/vncwebsocket',
+    ticket:
+      'PVEVNC:643D360D::d0UyG/TdIWJSOM1UAHrPbnCwKip6SWRNpt1f4cOgpnJvP9KrY2Y5J+wzIPDM/sCnHAdYufpk2Il7i5kyYUmujw52vxHLuAxC89TiS4soWPC7c5XcHFQ11P9nacwpH1NEzlxEIb9FpVc6vH0t9Lpf5ceYLil1XMZB254TrvMyUk18IbaNOhtqUHtmT2Wxy+UwcNPlDA7tiAc3uqjk1BCE3cPonf8BMzYKNA7tszFAksWdTzcFYnYowrbvdnDEOUriM0MyvFL57JEbQBq1kvPQuXiBZ1J9mkLqhXphXOH6blApv1U+Ndtuq4y3vlOOe4WRUaVvSdP1Zykt6T0etflviw==',
+  }
+  const apiUrl = JSON.stringify(mockData)
 
   return (
     <>
